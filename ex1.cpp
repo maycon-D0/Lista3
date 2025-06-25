60521c8caa3e38e0d9f8c1049e51720d35eca1c0
maycon djeison <s.maycon@aluno.ifsp.edu.br>
2025-06-25 20:17:47 -0300

    Add files via upload

---
+++ ex1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i;
+	do
+	{
+	cout<<"digite um valor:"<<endl;
+	cin>>i;
+	if( i>0)
+	
+	    cout << i << " o valor e positivo" << endl;
+	}
+	while (i >0);	
+	return 0;
+}
